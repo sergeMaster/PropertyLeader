65a595cf18288b7f2c253c5d077dcbf27eec5a84
mengaptche <martialserge26@gmail.com>
2015-03-19 13:01:22 +0200

    Test

--- app/src/main/java/za/co/fnb/propertyleader/codefest/propertyleader/MainActivity.java
+++ app/src/main/java/za/co/fnb/propertyleader/codefest/propertyleader/MainActivity.java
@@ -131,7 +131,7 @@ public class MainActivity extends ActionBarActivity
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_buy, container, false);
             return rootView;
         }
 
